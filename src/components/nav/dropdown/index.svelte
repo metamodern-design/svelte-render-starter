cbb7190726c7d3c0c48aa5df4da290188dddf746
Daniel Narey <contact@danielnarey.com>
2021-01-01 17:03:52 -0500

    sync

--- src/components/nav/dropdown/index.svelte
+++ src/components/nav/dropdown/index.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { scale } from 'svelte/transition';
+  import { cubicIn, cubicOut } from 'svelte/easing';
   import Links from '$/nav/dropdown/links.svelte';
   
   export let alt = 'Open menu';
@@ -9,11 +11,16 @@
   const toggle = () => {
     isOpen = !isOpen;
   };
+  
+  const transition = {
+    in: { duration: 100, start: 0.95, easing: cubicOut },
+    out: { duration: 75, start: 0.95, easing: cubicIn },
+  };
 
 </script>
 
 <template lang="pug">
-  .relative-container
+  .dropdown-container
     div
       button(
         id="user-menu"
@@ -24,11 +31,13 @@
 
         slot(name="trigger")
 
-    .popup-menu(
+    .dropdown-panel(
       role="menu"
       aria-orientation="vertical"
       aria-labelledby="user-menu"
       class:active= '{isOpen === true}'
+      in:scale= '{transition.in}'
+      out:scale= '{transition.out}'
     )
       Links('{items}')
 
@@ -44,7 +53,7 @@
 </template>
 
 <style lang="sugarss">
-  .relative-container
+  .dropdown-container
     @apply relative
     @apply ml-3
     
@@ -66,14 +75,14 @@
   button > :global(svg)
     @apply h-8 w-8 
     
-  .popup-menu
+  .dropdown-panel
     @apply hidden
     @apply absolute origin-top-right right-0
     @apply w-48 mt-2 py-1 
     @apply bg-white rounded-md shadow-lg
     @apply ring-1 ring-black ring-opacity-5
   
-  .popup-menu.active
+  .dropdown-panel.active
     @apply block
   
 </style>
